2ffc8494f965f653d3a982cd5c09551f7a3fbdd3
Lidia Martinez <lidia.martinez@avvale.com>
2024-01-29 11:39:40 +0100

    store screen

--- lib/features/shop/widgets/vertical_image_text.dart
+++ lib/features/shop/widgets/vertical_image_text.dart
@@ -8,52 +8,55 @@ class CVerticalImageText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: CSizes.defaultSpace),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Popular categories',
-            style: Theme.of(context).textTheme.headlineSmall,
-            
-          ),
-          const SizedBox(
-            height: CSizes.spaceBtwItems,
-          ),
-           SizedBox(
-            height: 80,
-             child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 6,
-               scrollDirection: Axis.horizontal,
-               itemBuilder: ((_, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(right: CSizes.spaceBtwItems),
-                  child: Column(
-                     children: [
-                       Container(
-                         width: 56,
-                         height: 56,
-                         padding: const EdgeInsets.all(CSizes.sm),
-                         decoration: BoxDecoration(
-                           color: CColors.white,
-                           borderRadius: BorderRadius.circular(100),
-                         ),
-                         child: const Center(
-                          child: Image(image: AssetImage(CImages.shoeIcon), fit: BoxFit.cover, color: CColors.white,),
-                          )
-                         ),
-                         const SizedBox(height: CSizes.spaceBtwItems/2),
-                         Text('Shoes', style: Theme.of(context).textTheme.labelMedium!.apply(color: CColors.white),)
-                     ],
-                   ),
-                );
-               }),
-               
-             ),
-           )
-        ],
+    return GestureDetector(
+      onTap: (){},
+      child: Padding(
+        padding: const EdgeInsets.only(left: CSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Popular categories',
+              style: Theme.of(context).textTheme.headlineSmall,
+              
+            ),
+            const SizedBox(
+              height: CSizes.spaceBtwItems,
+            ),
+             SizedBox(
+              height: 80,
+               child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 6,
+                 scrollDirection: Axis.horizontal,
+                 itemBuilder: ((_, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: CSizes.spaceBtwItems),
+                    child: Column(
+                       children: [
+                         Container(
+                           width: 56,
+                           height: 56,
+                           padding: const EdgeInsets.all(CSizes.sm),
+                           decoration: BoxDecoration(
+                             color: CColors.white,
+                             borderRadius: BorderRadius.circular(100),
+                           ),
+                           child: const Center(
+                            child: Image(image: AssetImage(CImages.shoeIcon), fit: BoxFit.cover, color: CColors.white,),
+                            )
+                           ),
+                           const SizedBox(height: CSizes.spaceBtwItems/2),
+                           Text('Shoes', style: Theme.of(context).textTheme.labelMedium!.apply(color: CColors.white),)
+                       ],
+                     ),
+                  );
+                 }),
+                 
+               ),
+             )
+          ],
+        ),
       ),
     );
   }
